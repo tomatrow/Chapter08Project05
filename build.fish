e8c3369ce388e22b2e29b3f130c7a382438c4046
AJ Caldwell <tomatrow@gmail.com>
2014-02-06 03:20:09 -0800

    Now builds markdown doc

--- build.fish
+++ build.fish
@@ -1,12 +1,18 @@
 #!/usr/local/bin/fish
 set SRC_DIR $PWD/src
 set BLD_DIR $PWD/bld
-set MAIN_CLASS Chapter08Project05
+set SRC_FILES $SRC_DIR/*.java
+set MAIN_CLASS Chapter08Project05.java
+set BASE_MAIN (basename $MAIN_CLASS .java)
 
 if test $argv = "build" 
-        echo Building
-        javac -d $BLD_DIR $SRC_DIR/*.java
+    echo Building
+    javac -d $BLD_DIR $SRC_FILES
+	codemark $SRC_DIR/$MAIN_CLASS $SRC_FILES > $BLD_DIR/$MAIN_CLASS.md
 else if test $argv = "run"
-        echo Running 
-        java -cp $BLD_DIR $MAIN_CLASS
+    echo Running 
+    java -cp $BLD_DIR $MAIN_CLASS
+else if test $argv = "shoot"
+	echo Shooting 
+	screencapture -w $BLD_DIR/$BASE_MAIN.png 
 end
