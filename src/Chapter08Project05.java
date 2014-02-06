f7e58e837113a6ebd5ef0273d835d554a6acb45f
AJ Caldwell <tomatrow@gmail.com>
2014-02-06 02:27:29 -0800

    Finished project requirements

--- src/Chapter08Project05.java
+++ src/Chapter08Project05.java
@@ -2,13 +2,10 @@ import java.io.*;
 
 public class Chapter08Project05 {
 	public static void main(String[] args) {
-		System.out.println(System.getProperty("os.name"));
-		OutputStream s;
-		try {
-			Process p = Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", "tput "});
-		} catch (IOException e){
-			System.out.println(e);
-		}
-
+		Figure r = new Rectangle();
+		Figure t = new Triangle();
+		r.center();
+		System.out.println();
+		t.center();
 	}
 }
